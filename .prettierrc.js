1f43f7b06b796d447396ac768964966ba56954a0
gjlee <dlrlwjd56@gmail.com>
2025-10-21 23:43:12 +0900

    stack navigation

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-    arrowParens: "avoid",
-    bracketSameLine: true,
-    bracketSpacing: false,
-    jsingleQuote: true,
-    trailingComma: "all",
+  arrowParens: "avoid",
+  bracketSameLine: true,
+  bracketSpacing: false,
+  jsingleQuote: true,
+  trailingComma: "all",
 };
---
+++ app/browser.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {Text} from "react-native";
+
+const BrowserScreen = () => {
+  return <Text>BrowserScreen</Text>;
+};
+
+export default BrowserScreen;
