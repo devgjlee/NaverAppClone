6ce8005387f0c3748e12f28712f90c90a36ddaa5
gjlee <dlrlwjd56@gmail.com>
2025-10-27 20:38:27 +0900

    웹뷰 홈화면 임포트 및 브라우저 기능 구현

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,40 @@
 import React from "react";
-import {Text} from "react-native";
+import {WebView} from "react-native-webview";
+import {Platform, SafeAreaView, StatusBar, StyleSheet} from "react-native";
+import {router} from "expo-router";
+const styles = StyleSheet.create({
+  safearea: {
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    flex: 1,
+  },
+});
 
 const HomeScreen = () => {
-  return <Text>Home</Text>;
+  return (
+    <SafeAreaView style={styles.safearea}>
+      <WebView
+        source={{uri: "https://m.naver.com"}}
+        showsVerticalScrollIndicator={false}
+        onShouldStartLoadWithRequest={request => {
+          console.log("Home - request", request);
+
+          if (
+            request.url.startsWith("https://m.naver.com") ||
+            request.mainDocumentURL?.startsWith("https://m.naver.com")
+          ) {
+            return true;
+          }
+          if (request.url != null && request.url.startsWith("https://")) {
+            router.navigate({
+              pathname: "browser",
+            });
+            return false;
+          }
+          return true;
+        }}
+      />
+    </SafeAreaView>
+  );
 };
 
 export default HomeScreen;
